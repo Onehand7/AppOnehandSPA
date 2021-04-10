7981a64a259a8949fe03d0df584b4c95130dcf5b
sebastian arenas <ceducproyecto7@gmail.com>
2021-04-10 01:43:20 -0400

    Pantalla Splash Screen creada

---
+++ app/src/main/java/com/example/app_onehandspa/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.example.app_onehandspa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        startTime()
+    }
+
+    private fun startTime(){
+        object: CountDownTimer(3000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+               val intent = Intent(applicationContext, MainActivity::class.java).apply{}
+                startActivity(intent)
+            }
+
+        }.start()
+    }
+}
