0a4a15efc5e406025905020aa9487ac310d95c22
sebastian arenas <ceducproyecto7@gmail.com>
2021-04-15 09:45:08 -0400

    Se cambiaron los id del formulario de registro y el Intent en RegistroActivity

--- app/src/main/java/com/example/app_onehandspa/RegistroActivity.kt
+++ app/src/main/java/com/example/app_onehandspa/RegistroActivity.kt
@@ -1,11 +1,23 @@
 package com.example.app_onehandspa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        setup()
+    }
+    private fun setup(){
+        val btn_siguiente = findViewById<Button>(R.id.siguienteButton)
+
+        btn_siguiente.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
