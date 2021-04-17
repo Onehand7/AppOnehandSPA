e9f0c8a16463be2d0e629f38c833a4dcd849ef8e
sebastian arenas <ceducproyecto7@gmail.com>
2021-04-17 18:20:04 -0400

    Se configuro las VALIDACIONES en Login y Registro

--- app/src/main/java/com/example/app_onehandspa/LoginActivity.kt
+++ app/src/main/java/com/example/app_onehandspa/LoginActivity.kt
@@ -3,12 +3,19 @@ package com.example.app_onehandspa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PatternMatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.util.PatternsCompat
 import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -32,9 +39,14 @@ class LoginActivity : AppCompatActivity() {
                     if (it.isSuccessful){
                         showMain(it.result?.user?.email ?:"",ProvideType.BASIC)
                     }else{
-                        showAlert()
+                        //Toast.makeText(this,"Usuario o contraseña incorrectos.", Toast.LENGTH_SHORT).show()
+                        validate()
                     }
                 }
+            }else{
+                //MENSAJE EMERGENTE CON CLASE TOAST
+                //Toast.makeText(this,"Rellene los campos", Toast.LENGTH_SHORT).show()
+                validate()
             }
 
         }
@@ -43,14 +55,6 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-    private fun showAlert(){
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error AUTENTICANDO al usuario")
-        builder.setPositiveButton("Aceptar",null)
-        val dialog: AlertDialog =builder.create()
-        dialog.show()
-    }
     private fun showMain(email:String,provider: ProvideType){
         val mainIntent = Intent(this,MainActivity::class.java).apply {
             putExtra("email",email)
@@ -58,6 +62,51 @@ class LoginActivity : AppCompatActivity() {
         }
         startActivity(mainIntent)
     }
+    private fun validate(){
+        val result = arrayOf(validateEmail(),validatePassword())
+        if (false in result){
+            return
+        }
+        Toast.makeText(this,"Exito",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun validateEmail():Boolean{
+        val email = findViewById<EditText>(R.id.emailEditText)
+        return when {
+            email.text.isEmpty() -> {
+                email.error = " los campos no pueden estar vacios"
+                false
+            }
+            !PatternsCompat.EMAIL_ADDRESS.matcher(email.text.toString()).matches() -> {
+                email.error = "escriba un correo electronico valido"
+                false
+            }
+            else -> {
+                email.error = null
+                true
+            }
+        }
+    }
+    private fun validatePassword():Boolean{
+        val password = findViewById<EditText>(R.id.passwordEditText)
+        val passwordRegex = Pattern.compile(
+            "^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}\$"
+        )
+        return when {
+            password.text.isEmpty() ->{
+                password.error = " los campos no pueden estar vacios"
+                false
+            }!passwordRegex.matcher(password.text.toString()).matches() -> {
+                password.error = "Al menos 1 dígito, 1 minúscula, 1 mayúscula, 8-16 caracteres."
+                false
+            }
+            else -> {
+                password.error = null
+                true
+            }
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/app_onehandspa/RegistroActivity.kt
+++ app/src/main/java/com/example/app_onehandspa/RegistroActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.util.PatternsCompat
 import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,29 +27,24 @@ class RegistroActivity : AppCompatActivity() {
         val btn_siguiente = findViewById<Button>(R.id.siguienteButton)
 
         btn_siguiente.setOnClickListener {
-            if (emailRegistroText.text.isNotEmpty() && passRepText.text.isNotEmpty()){
+            if (nombreText.text.isNotEmpty() && emailRegistroText.text.isNotEmpty()&& passwordText.text.isNotEmpty() && passRepText.text.isNotEmpty()){
                 // SE CREA LA INSTANCA DE FIREBASEAUTH PERMITIENDO CREAR UN USUSARIO POR EMAIL Y PASSWORD
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(
                     emailRegistroText.text.toString(),
                     passRepText.text.toString()
                 ).addOnCompleteListener{
-                    if (it.isSuccessful){
+                    if (it.isSuccessful && passRepText.text.toString() == passwordText.text.toString()){
                         showMain(it.result?.user?.email ?:"",ProvideType.BASIC)
                     }else{
-                        showAlert()
+                        Toast.makeText(this,"Correo o contraseña erronea", Toast.LENGTH_SHORT).show()
                     }
                 }
+            }else{
+                //Toast.makeText(this,"Rellene los campos", Toast.LENGTH_SHORT).show()
+                validate()
             }
         }
 
-    }
-    private fun showAlert(){
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error AUTENTICANDO al usuario")
-        builder.setPositiveButton("Aceptar",null)
-        val dialog:AlertDialog=builder.create()
-        dialog.show()
     }
     private fun showMain(email:String,provider: ProvideType){
         val mainIntent = Intent(this,MainActivity::class.java).apply {
@@ -55,4 +53,86 @@ class RegistroActivity : AppCompatActivity() {
         }
         startActivity(mainIntent)
     }
+
+
+    private fun validate(){
+
+        val result = arrayOf(validateNombre(),validateEmail(),validatePassword(),validateRepPassword())
+        if (false in result){
+            return
+        }
+
+        Toast.makeText(this,"Exito",Toast.LENGTH_SHORT).show()
+    }
+    private fun validateNombre():Boolean{
+        val nombre = findViewById<EditText>(R.id.nombreEditText)
+        return when {
+            nombre.text.isEmpty() -> {
+                nombre.error = " los campos no pueden estar vacios"
+                false
+            }
+            else -> {
+                nombre.error = null
+                true
+            }
+        }
+    }
+
+    private fun validateEmail():Boolean{
+        val email = findViewById<EditText>(R.id.emailRegistroEditText)
+        return when {
+            email.text.isEmpty() -> {
+                email.error = " los campos no pueden estar vacios"
+                false
+            }
+            !PatternsCompat.EMAIL_ADDRESS.matcher(email.text.toString()).matches() -> {
+                email.error = "escriba un correo electronico valido"
+                false
+            }
+            else -> {
+                email.error = null
+                true
+            }
+        }
+    }
+    private fun validatePassword():Boolean{
+        val password = findViewById<EditText>(R.id.passwordRegistroEditText)
+        val passwordRegex = Pattern.compile(
+            "^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}\$"
+        )
+        return when {
+            password.text.isEmpty() ->{
+                password.error = " los campos no pueden estar vacios"
+                false
+            }!passwordRegex.matcher(password.text.toString()).matches() -> {
+                password.error = "Al menos 1 dígito, 1 minúscula, 1 mayúscula, 8-16 caracteres."
+                false
+            }
+            else -> {
+                password.error = null
+                true
+            }
+        }
+    }
+    private fun validateRepPassword():Boolean{
+        val passwordRep = findViewById<EditText>(R.id.passwordRepRegistroEditText)
+        val passwordRegex = Pattern.compile(
+            "^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}\$"
+        )
+        return when {
+            passwordRep.text.isEmpty() ->{
+                passwordRep.error = " los campos no pueden estar vacios"
+                false
+            }!passwordRegex.matcher(passwordRep.text.toString()).matches() -> {
+                passwordRep.error = "Al menos 1 dígito, 1 minúscula, 1 mayúscula, 8-16 caracteres."
+                false
+            }
+            else -> {
+                passwordRep.error = null
+                true
+            }
+        }
+    }
+
+
 }
